784cd1e894cd3b28154518295f32624403991c73
Michael Poltronieri Tang <mtang8264@gmail.com>
2026-04-08 12:41:36 -0400

    Changed grandfather clock pendulum motion to being code controlled for simplicity. Also fixed visible chain extending below weight.

--- Scripts/GrandfatherClockController.gd
+++ Scripts/GrandfatherClockController.gd
@@ -1,28 +1,29 @@
 extends Node
 
+var timer := 0.5
+
 @export var hour: int = 2
 @export var minute: float = 55
 @export var pendulum_swing: bool = true
+@export var pendulum_speed_multiplier: float = 1.0
 
 @onready var hour_hand := find_child("HourHand")
 @onready var minute_hand := find_child("MinuteHand")
-@onready var anim : AnimationPlayer= find_child("AnimationPlayer")
+@onready var pendulum := find_child("Pendulum")
+
+const pendulum_range := 3.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#anim.play("PendulumAction")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if pendulum_swing and anim.is_playing() == false:
-		anim.play("PendulumAction")
-	elif not pendulum_swing:
-		anim.pause()
-	
 	rotate_hour_hand()
 	rotate_minute_hand()
+	if pendulum_swing:
+		swing_pendulum(delta)
 	pass
 
 func rotate_hour_hand() -> void:
@@ -35,4 +36,10 @@ func rotate_hour_hand() -> void:
 func rotate_minute_hand() -> void:
 	var target_rotation = 360 - ( minute / 60 * 360 )
 	minute_hand.rotation_degrees.z = lerp(minute_hand.rotation_degrees.z, target_rotation, 0.1)
+	pass
+	
+func swing_pendulum(delta: float) -> void:
+	timer += delta
+	var mag = cos(PI * timer) * pendulum_range
+	pendulum.rotation_degrees.z = mag
 	pass
