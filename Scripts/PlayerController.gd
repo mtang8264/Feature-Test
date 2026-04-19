de0e0443ffa188478a8c273f30f422cad2219cfc
Michael Poltronieri Tang <mtang8264@gmail.com>
2026-04-19 12:35:55 -0400

    Added a raycast to the player controller which checks for objects that are marked interactable. Marked several objects as possible interactables.

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -6,11 +6,16 @@ const JUMP_VELOCITY = 10
 const TURN_SPEED = 0.25
 const NOD_SPEED = 0.1
 
+const INTERACTION_MASK = 16
+const INTERACTION_RAY_LENGTH = 2.0
+
 @onready var cam : Camera3D = find_child("Camera3D")
 
 func _physics_process(delta: float) -> void:
 	if GameController.current_state == GameController.GAME_STATES.WALKING and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		_player_movement(delta)
+		
+	print(_interactable_check())
 
 func _input(event: InputEvent) -> void:
 	# Mouse motion that translates to camera movement
@@ -51,3 +56,12 @@ func _camera_move(motion: Vector2):
 	var nod := -1 * deg_to_rad(motion.y * NOD_SPEED)
 	cam.rotate_x(nod)
 	pass
+
+func _interactable_check() -> Dictionary:
+	var space_state = get_world_3d().direct_space_state
+	var origin = cam.global_position
+	var end = origin - cam.global_basis.z * INTERACTION_RAY_LENGTH
+	var query = PhysicsRayQueryParameters3D.create(origin, end, INTERACTION_MASK)
+	query.collide_with_areas = true
+	var results = space_state.intersect_ray(query)
+	return results
